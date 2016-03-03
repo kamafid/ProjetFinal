5bb030325d201ec3364a220541e0445ff557b453
kamafid <elhafid@hotmail.com>
2016-03-04 00:06:40 +0100

    ajout des imports necessaires

--- ConnectionBDD.java
+++ ConnectionBDD.java
@@ -1,7 +1,11 @@
 package com.sdz.connection;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
 
 public class SdzConnection{
 
